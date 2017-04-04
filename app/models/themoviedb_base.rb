fb30a6351e391af94aeb65c5416baf7976cfab92
Nina Witek <nina.witek@gmail.com>
2017-04-04 20:46:17 +0200

    Prettify displaying info from themoviedb

--- app/models/themoviedb_base.rb
+++ app/models/themoviedb_base.rb
@@ -1,10 +1,18 @@
 module ThemoviedbBase
+  def self.config
+    @config = Tmdb::Configuration.get
+  end
+
   def tmdb_movie
     @tmdb_movie ||= Tmdb::Search.movie(title).results.first
   end
 
-  def poster
+  def poster_path
     tmdb_movie.poster_path
+  end
+
+  def poster_url(size)
+    ThemoviedbBase.config.images.secure_base_url + size.to_s + poster_path
   end
 
   def avg_rating
